ca8a9eb13291c70e0b84f646fcb2afc22276f1d6
Jeroen Lafleur <jeroenlafleur@gmail.com>
2024-10-09 13:28:29 +0200

    Add tests for CeasarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,36 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    private CaesarShiftCipher ceasarShiftCipher;
+
+    @BeforeEach
+    public void initialize() {
+        ceasarShiftCipher = new CaesarShiftCipher();
+    }
+
+    @ParameterizedTest(name = "message={0}, shift={1}, expected={2}")
+    @CsvSource({"xyz,-3,uvw", "qrs,3,tuv"})
+    public void shiftInCurrentRange(String message, int shift, String expected) {
+        String result = ceasarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest(name = "message={0}, shift={1}, expected={2}")
+    @CsvSource({"xyz,-29,uvw", "qrs,29,tuv"})
+    public void shiftOutsideCurrentRange(String message, int shift, String expected) {
+        String result = ceasarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest(name = "message={0}, shift={1}, expected={2}")
+    @CsvSource({"123,1,invalid", "@#$,1,invalid", ",1,invalid"})
+    public void invalidMessage(String message, int shift, String expected) {
+        String result = ceasarShiftCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expected, result);
+    }
 }
