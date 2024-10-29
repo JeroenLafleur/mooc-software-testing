22b9a3f4a4f7492f5a99da2e06a87ddaaa72c742
Jeroen Lafleur <jeroenlafleur@gmail.com>
2024-10-29 11:02:39 +0100

    Add tests for Mirror

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,64 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+    private Mirror mirror;
+
+    @BeforeEach
+    public void Initialize() {
+        mirror = new Mirror();
+    }
+
+    @Test
+    public void noMirrorableEnds() {
+        String noMirrorableEnds = "mirror";
+
+        String actual = mirror.mirrorEnds(noMirrorableEnds);
+
+        String expected = "";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void evenLengthWithMirrorableEnds() {
+        String evenLengthWithMirrorableEnds = "abcDEFGcba";
+
+        String actual = mirror.mirrorEnds(evenLengthWithMirrorableEnds);
+
+        String expected = "abc";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void unevenLengthWithMirrorableEnds() {
+        String unevenLengthWithMirrorableEnds = "abcDEFcba";
+
+        String actual = mirror.mirrorEnds(unevenLengthWithMirrorableEnds);
+
+        String expected = "abc";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void evenLengthWithOverlappingMirrorableEnds() {
+        String evenLengthWithOverlappingMirrorableEnds = "abba";
+
+        String actual = mirror.mirrorEnds(evenLengthWithOverlappingMirrorableEnds);
+
+        String expected = "abba";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void unevenLengthWithOverlappingMirrorableEnds() {
+        String unevenLengthWithOverlappingMirrorableEnds = "abbba";
+
+        String actual = mirror.mirrorEnds(unevenLengthWithOverlappingMirrorableEnds);
+
+        String expected = "abbba";
+        Assertions.assertEquals(expected, actual);
+    }
 }
